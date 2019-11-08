e096f78244a9bc6e94b2b9b35186dcb07f10800d
Caroline Hughes <clhughes@berkeley.edu>
2019-11-08 15:50:41 -0700

    added bea_detailed

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SLiDE
 
-Download [datasources.zip](https://windc.wisc.edu/datastream.html) and add this to [data](https://github.com/NREL/SLiDE/tree/master/data) to run parse.jl
+Download
+[datasources.zip](https://windc.wisc.edu/datastream.html) and add this to
+[data](https://github.com/NREL/SLiDE/tree/master/data) to run
+[parse.jl](https://github.com/NREL/SLiDE/blob/master/data/parse.jl).
--- data/parse.jl
+++ data/parse.jl
@@ -91,11 +91,135 @@ function map_with_dataframe(input, mapfile::String;
     read from a map stored in a .csv file.
     """
 
-    df_map = CSV.read("core_maps/$mapfile.csv")
+    # CSV.read() called with 'silencewarnings' enabled to prevent warnings
+    # for empty cells. These will be read as 'missing'.
+    df_map = CSV.read("core_maps/$mapfile.csv", silencewarnings = true)
     dict_map = Dict(k => v for (k, v) in zip(df_map[!, from], df_map[!, to]))
 
     output = map(x -> dict_map[x], input);
     return output
+end
+
+function map_with_dataframe(input, mapfile::DataFrame;
+    from::Symbol = :from, to::Symbol = :to)
+    """
+    This function maps a dataframe column into a new column based on values
+    read from a map stored in a .csv file.
+    """
+
+    df_map = mapfile
+    dict_map = Dict(k => v for (k, v) in zip(df_map[!, from], df_map[!, to]))
+
+    # println(input)
+    # println("TRADE" in input)
+
+    output = map(x -> dict_map[x], input);
+    return output
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_renaming(df::DataFrame, y::Dict)
+    """
+    This function renames columns 'from' -> 'to'.
+    """
+    rename!(df, [d["from"] => d["to"] for d in y["renaming"]])
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_melting(df::DataFrame, y::Dict)
+    """
+    Normalize the dataframe by 'melting' columns into rows.
+    This will lengthen the dataframe by duplicating values in the column 'on'
+    into new rows. This will define two new columns.
+      (1) 'var' (of 'type') with header names from the original dataframe.
+      (2) 'val' with column values from the original dataframe.
+    """
+    for d in y["melting"]
+        df = melt(df, d["on"], variable_name = d["var"], value_name = d["val"])
+        df[!, d["var"]] = convert_type(d["type"], df[!, d["var"]])
+    end
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_setting(df::DataFrame, y::Dict)
+    """
+    Define a column 'col' and set all elements to the value 'val'.
+    """
+    [df[!, d["col"]] .= d["val"] for d in y["setting"]]
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_mapping(df::DataFrame, y::Dict)
+    """
+    Define an 'output' column containing values based on those in an
+    'input' column. The mapping columns 'from' -> 'to' are contained in a
+    .csv 'file' in the core_maps directory. The columns 'input' and 'from'
+    should contain the same values, as should 'output' and 'to'.
+    """
+    # CSV.read() called with 'silencewarnings' enabled to prevent warnings
+    # for empty cells. These will be read as 'missing'.
+    df_maps = Dict(k => CSV.read("core_maps/$k.csv", silencewarnings = true)
+        for k in unique([v for d in y["mapping"] for v in [d["file"]]]))
+    
+    for d in y["mapping"]
+        df[!, d["input"]]  .= string.(strip.(df[!, d["input"]]))
+        df[!, d["output"]] .= map_with_dataframe(df[!, d["input"]], df_maps[d["file"]];
+            from = d["from"], to = d["to"])
+
+        # This code works if the dictionary of map dataframes was NOT
+        # defined, using the method of map_with_dataframe() that takes a
+        # dataframe as an argument in place of the core_map file name.
+        # df[!, d["output"]] .= map_with_dataframe(df[!, d["input"]], d["file"];
+        #     from = d["from"], to = d["to"])
+    end
+
+    # This code can replace the for loop if the values to replace have
+    # already been stripped of excess white space.
+    # [df[!, d["output"]] .= map_with_dataframe(df[!, d["input"]], d["file"];
+        # from = d["from"], to = d["to"]) for d in y["mapping"]]
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_replacing(df::DataFrame, y::Dict)
+    """
+    Replace values 'from' in the column 'col' with values 'to'.
+    """
+    # # This works if there are no missing values in the column to replace.
+    # [df[!, d["col"]][df[:, d["col"]] .== d["from"]] .= d["to"]
+    #     for d in y["replacing"]]
+    
+    # This is clunkier, but works when there are missing values. Other options:
+    # (1) Create a separate "missing" dictionary in the YAML read file to
+    #     handle missing values before attempting to replace other values.
+    # (2) Automatically set missing values to 0 in this function.
+    [df[!, d["col"]][occursin.(string(d["from"]), convert_type("string", df[:, d["col"]]))] .= d["to"]
+        for d in y["replacing"]]
+
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_appending(df::DataFrame, df_temp::DataFrame, y::Dict, f::Dict)
+    [df_temp[!, d["col"]] .= f[d["val"]] for d in y["appending"]]
+    df = vcat(df, df_temp)
+    return df
+end
+
+# ------------------------------------------------------------------------------
+function dataframe_reordering(df::DataFrame, y::Dict)
+
+    # Reorder columns.
+    cols = [Symbol(k) for c in y["col_out"] for k in keys(c)]
+    df = df[!, cols]
+
+    # Set columns to the correct type.
+    types = Dict(Symbol(k) => v for c in y["col_out"] for (k,v) in c)
+    [df[!, c] .= convert_type(types[c], df[!, c]) for c in cols]
+    return df
 end
 
 # ------------------------------------------------------------------------------
@@ -103,45 +227,11 @@ function edit_dataframe_from_yaml(df::DataFrame, y::Dict)
     """
     """
     
-    # RENAME -- Columns 'from' -> 'to'.
-    try;   rename!(df, [d["from"] => d["to"] for d in y["renaming"]])
-    catch; nothing
-    end
-
-    # MELT -- Normalize the dataframe by 'melting' columns into rows.
-    # This will lengthen the dataframe by duplicating values in the column 'on'
-    # into new rows. This will define two new columns.
-    #   (1) 'var' (of 'type') with header names from the original dataframe.
-    #   (2) 'val' with column values from the original dataframe.
-    try
-        for d in y["melting"]
-            df = melt(df, d["on"], variable_name = d["var"], value_name = d["val"])
-            df[!, d["var"]] = convert_type(d["type"], df[!, d["var"]])
-        end
-    catch; nothing
-    end
-
-    # SET -- Define a column 'col' and set all elements to the value 'val'.
-    try;   [df[!, d["col"]] .= d["val"] for d in y["setting"]]
-    catch; nothing
-    end
-
-    # MAP -- Define an 'output' column containing values based on those in an
-    # 'input' column. The mapping columns 'from' -> 'to' are contained in a
-    # .csv 'file' in the core_maps directory. The columns 'input' and 'from'
-    # should contain the same values, as should 'output' and 'to'.
-    try
-        [df[!, d["output"]] .= map_with_dataframe(df[!, d["input"]], d["file"];
-            from = d["from"], to = d["to"]) for d in y["mapping"]]
-    catch; nothing
-    end
-
-    # REPLACE -- Values 'from' in the column 'col' with values 'to'.
-    try
-        [df[!, d["col"]][df[:, d["col"]] .== d["from"]] .= d["to"]
-            for d in y["replacing"]]
-    catch; nothing
-    end
+    if "renaming" in keys(y);  df = dataframe_renaming(df, y);  end
+    if "melting" in keys(y);   df = dataframe_melting(df, y);   end
+    if "setting" in keys(y);   df = dataframe_setting(df, y);   end
+    if "mapping" in keys(y);   df = dataframe_mapping(df, y);   end
+    if "replacing" in keys(y); df = dataframe_replacing(df, y); end
 
     return df
 end
@@ -150,35 +240,61 @@ end
 #               MAIN
 # ------------------------------------------------------------------------------
 yaml_structure = YAML.load(open("readfiles/read_structure.yml"))
+yaml_all = YAML.load(open("readfiles/read_all.yml"))
 
-y = read_yaml("read_bea_supply");
-# y = read_yaml("read_bea_use");
-# y = read_yaml("read_crude_oil");
-# y = read_yaml("read_emissions");
-# y = read_yaml("read_heatrate");
+for yaml in yaml_all["file"]
 
-df = DataFrame()
+    println("reading ", yaml)
 
-for f in y["file_in"]
+    y = read_yaml(yaml);
+    df = DataFrame()
 
-    df_temp = read_dataframe_from_yaml(y["path_in"], f);
-    df_temp = edit_dataframe_from_yaml(df_temp, y);
-    df_temp = first(df_temp, 3);
+    for f in y["file_in"]
 
-    try;   [df_temp[!, d["col"]] .= f[d["val"]] for d in y["appending"]];
-    catch; nothing
+        df_temp = read_dataframe_from_yaml(y["path_in"], f);
+        df_temp = edit_dataframe_from_yaml(df_temp, y);
+        
+        if "appending" in keys(y); df = dataframe_appending(df, df_temp, y, f)
+        else;                      df = df_temp
+        end
+
     end
 
-    global df = vcat(df, df_temp)
+    df = dataframe_reordering(df, y)
+    CSV.write(string(yaml_all["path_out"], "/" ,y["file_out"]), df)
 
 end
 
-# Reorder columns.
-cols = [Symbol(k) for c in y["col_out"] for k in keys(c)]
-df = df[!, cols]
 
-# Set columns to the correct type.
-types = Dict(Symbol(k) => v for c in y["col_out"] for (k,v) in c)
-[df[!, c] .= convert_type(types[c], df[!, c]) for c in cols]
 
-df
+
+
+
+
+
+
+
+# # y = read_yaml("read_bea_supply");
+# # y = read_yaml("read_bea_use");
+# # y = read_yaml("read_bea_use_detailed");
+# # y = read_yaml("read_bea_supply_detailed");
+# # y = read_yaml("read_crude_oil");
+# # y = read_yaml("read_emissions");
+# # y = read_yaml("read_heatrate");
+
+# df = DataFrame()
+
+# for f in y["file_in"]
+
+#     df_temp = read_dataframe_from_yaml(y["path_in"], f);
+#     df_temp = first(df_temp, 3)
+
+#     df_temp = edit_dataframe_from_yaml(df_temp, y);
+    
+#     if "appending" in keys(y); global df = dataframe_appending(df, df_temp, y, f)
+#     else;                      global df = df_temp
+#     end
+
+# end
+
+# df = dataframe_reordering(df, y)
