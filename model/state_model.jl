a9671dcd85f5efa62eb7d74e78843fb83698a4bf
Maxwell Brown <34488378+maxxb77@users.noreply.github.com>
2020-02-12 14:50:04 -0700

    Adding functions to convert dataframes into vectors of tuples for conditional variable creation; first successful variable creation

--- model/state_model.jl
+++ model/state_model.jl
@@ -4,10 +4,13 @@
 #
 ################################################
 
+
+#include("../src/slide.jl")
 using SLiDE
 using CSV
 using JuMP
 using Complementarity
+using DataFrames
 
 ############
 # LOAD DATA
@@ -18,15 +21,18 @@ using Complementarity
 
 # First need to select a year for the model to be based off of
 mod_year = 2014
+
+#specify the path where the dumped csv files are stored
 data_temp_dir = abspath(joinpath(dirname(Base.find_package("SLiDE")), "..", "model", "data_temp"))
 
+#function here simplifies the loading and subsequent subsetting of the dataframes
 function read_data_temp(file::String,year::Int64,dir::String,desc::String)
     df = SLiDE.read_file(dir,CSVInput(name=string(file,".csv"),descriptor=desc))
     df = df[df[!,:yr].==year,:]
     return df
 end
 
-
+#blueNOTE contains a dictionary of the parameters needed to specify the model
 blueNOTE = Dict(
     :ys0 => read_data_temp("ys0",mod_year,data_temp_dir,"Sectoral supply"),
     :id0 => read_data_temp("id0",mod_year,data_temp_dir,"Intermediate demand"),
@@ -56,34 +62,45 @@ blueNOTE = Dict(
     :nd0 => read_data_temp("nd0",mod_year,data_temp_dir,"Regional demand from national market")
 )
 
-```
-function unique_array(dict::Dict,symb::Symbol)
-    k = unique(Dict[:symb],:symb)[!,:symb]
-    return k
-end
-```
-
 ##########
 # SETS
 ##########
 
 # extract model indices from blueNOTE dict
 # here are the exhaustive struct names
-s = unique(blueNOTE[:ys0],:s)[!,:s]
-g = unique(blueNOTE[:ys0],:g)[!,:g]
-r = unique(blueNOTE[:ys0],:r)[!,:r]
-m = unique(blueNOTE[:md0],:m)[!,:m]
+ss = unique(blueNOTE[:ys0],:s)[!,:s]
+gg = unique(blueNOTE[:ys0],:g)[!,:g]
+rr = unique(blueNOTE[:ys0],:r)[!,:r]
+mm = unique(blueNOTE[:md0],:m)[!,:m]
+
+
+function tupleize(z::DataFrames.DataFrame,sets::Vector)
+
+    #create an empty vector
+    v = []
+
+    #loop over elements in the vector 
+    for i in 1:length(z[!,sets[1]])
+        push!(v,[z[i,sets[1]],z[i,sets[2]]])
+    end #end loop
+
+    return v
+
+end #end functiobn
 
 # until we have structs, we'll need composite sets
-rs = unique(blueNOTE[:ys0][!,[:r,:s]])
-rg = unique(blueNOTE[:s0][!,[:r,:g]])
-a_rg = unique([blueNOTE[:a0];blueNOTE[:rx0]][!,[:r,:g]])
+# to evaluate the variable creation conditions
+y_rs = tupleize(unique(blueNOTE[:ys0][!,[:r,:s]]),[:r,:s])
+x_rg = tupleize(unique(blueNOTE[:s0][!,[:r,:g]]),[:r,:g]) #can be used as a substitute for pd0
+a_rg = tupleize(unique([blueNOTE[:a0];blueNOTE[:rx0]][!,[:r,:g]]),[:r,:g])
+pk_rs = tupleize(unique(blueNOTE[:kd0][!,[:r,:s]]),[:r,:s])
 
-kk = []
-for i in 1:length(rs[!,:r])
-    push!(kk,[rs[i,:r],rs[i,:s]])
+# function to check if the [x[xx],y[yy]] exists 
+# in xy where xy is a vector of tuples
+function check_xy(x,y,xx::Int64,yy::Int64,xy::Array)
+    tup = [x[xx],y[yy]]
+    return tup in xy
 end
-
 
 ################
 # VARIABLES
@@ -91,37 +108,26 @@ end
 
 cge = MCPModel();
 
-#convert rs to a vector of tuples
-kk=[]
-for i=1:length(rs[!,:r])
-    push!(kk,[rs[i,:r],rs[i,:s]])
-end
 
-#function to check if [x[xx],y[yy]] exists in xy
-function check_rs(x,y,xx::Int64,yy::Int64,xy::Array)
-    tup = [x[xx],y[yy]]
-    return tup in xy
-end
-
-@variable(cge, Y[rr=1:length(r),ss=1:length(s); check_rs(r,s,rr,ss,kk)]>=0)
-@variable(cge,X[r,g]>=0,start=1) # Disposition
-@variable(cge,A[r,g]>=0,start=1) # Absorption
-@variable(cge,C[r]>=0,start=1) # Aggregate final demand
-@variable(cge,MS[r,m]>=0,start=1) # Margin supply
+@variable(cge, Y[r=1:length(rr),ss=1:length(ss); check_xy(rr,ss,r,s,y_rs)]>=0)
+@variable(cge,X[r=1:length(rr),g=1:length(gg); check_xy(rr,gg,r,g,x_rg)]>=0,start=1) # Disposition
+@variable(cge,A[r=1:length(rr),g=1:length(gg); check_xy(rr,gg,r,g,a_rg)]>=0,start=1) # Absorption
+@variable(cge,C[r=1:length(rr)]>=0,start=1) # Aggregate final demand
+@variable(cge,MS[r=1:length(rr),m=1:length(mm)]>=0,start=1) # Margin supply
 
 #commodities:
-@variable(cge,PA[r,g]>=0,start=1) # Regional market (input)
-@variable(cge,PY[r,g]>=0,start=1) # Regional market (output)
-@variable(cge,PD[r,g]>=0,start=1) # Local market price
-@variable(cge,PN[g]>=0,start=1) # National market
-@variable(cge,PL[r]>=0,start=1) # Wage rate
-@variable(cge,PK[r,s]>=0,start=1) # Rental rate of capital
-@variable(cge,PM[r,m]>=0,start=1) # Margin price
-@variable(cge,PC[r]>=0,start=1) # Consumer price index
+@variable(cge,PA[r=1:length(rr),g=1:length(gg)]>=0,start=1) # Regional market (input)
+@variable(cge,PY[r=1:length(rr),g=1:length(gg)]>=0,start=1) # Regional market (output)
+@variable(cge,PD[r=1:length(rr),g=1:length(gg)]>=0,start=1) # Local market price
+@variable(cge,PN[g=1:length(gg)]>=0,start=1) # National market
+@variable(cge,PL[r=1:length(rr)]>=0,start=1) # Wage rate
+@variable(cge,PK[r=1:length(rr),s=1:length(ss)]>=0,start=1) # Rental rate of capital
+@variable(cge,PM[r=1:length(rr),m=1:length(ss)]>=0,start=1) # Margin price
+@variable(cge,PC[r=1:length(rr)]>=0,start=1) # Consumer price index
 @variable(cge,PFX>=0,start=1) # Foreign exchange
 
 #consumer:
-@variable(cge,RA[r]>=0,start=1) # Representative agent
+@variable(cge,RA[r=1:length(rr)]>=0,start=1) # Representative agent
 
 ##############
 # CONSTRAINTS
