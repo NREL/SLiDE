5e92b902157ff90e5d669d9ad2fe604d8063ff7a
Caroline L. Hughes <caroline.hughes@nrel.gov>
2020-12-10 15:07:06 -0700

    saving eem functions for eem pr

--- src/build/eem/module_elegen.jl
+++
@@ -1,9 +0,0 @@
-function module_elegen!(d::Dict, maps::Dict)
-    id = [:elegen,:heatrate]
-
-    df = indexjoin(d[:seds], maps[:elegen]; kind=:inner)
-    df = convertjoin(df, d[:heatrate]; id=id)
-    df = operate_with_2(df, maps[:operate]; id=id)
-    d[:elegen] = edit_with(df, Deselect([:sec],"=="))
-    return d[:elegen]
-end
