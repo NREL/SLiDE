d413405fb5b2c7bff1bb14806573ac60cc5607b5
Caroline L. Hughes <caroline.hughes@nrel.gov>
2021-01-20 13:21:06 -0700

    temporarily removed development directories

--- dev/runtime/runtime.jl
+++
@@ -1,7 +0,0 @@
-
-# https://discourse.julialang.org/t/very-slow-pre-compiling-and-using-of-custom-julia-package/24708
-# @time Base.convert(::Type{T}, cs::Array{DataFrame,1}) where {T<:Array}
-# @time Base.convert(::Type{Array}, cs::Array{C,1}) where C<:ChainDataFrame
-
-# @time convert_type(::Type{T}, x::Any) where T<:AbstractString = string(x)
-# @time convert_type(::Type{Array}, x::Any) = string(x)
