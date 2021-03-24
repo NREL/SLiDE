bee495097c1a32416616d4f3e60ef770ebf627de
Caroline L. Hughes <caroline.hughes@nrel.gov>
2021-03-24 17:43:23 -0600

    calibration DONE. This commit uses WiNDC parameter naming conventions (ex: s0 -> SUP). Subsequent versions will stick to SLiDE naming conventions (ex: s0 -> s0) for easier conversion back to DataFrames and consistency with model files. NOTE: this solves for 2016 with the exit message 'Converged to a point of local infeasibility. Problem may be infeasible.'

--- dev/ee_module/calibrate/setup.jl
+++ dev/ee_module/calibrate/setup.jl
@@ -1,13 +1,39 @@
 import Ipopt
 using JuMP
 
-year = 2016
-io_in = read_from("data/state_model/build/eem")
+global lb = 0.25
+global ub = 1.75
+global lb_seds = 0.75
+global ub_seds = 1.25
 penalty_nokey = SLiDE.DEFAULT_PENALTY_NOKEY
 
-# Read sets.
+year = 2016
+io_in = read_from(joinpath(SLIDE_DIR,"data","state_model","build","eem"))
+
+f_bench = joinpath(SLIDE_DIR,"dev","readfiles")
 f_read = joinpath(SLIDE_DIR,"src","build","readfiles")
 f_data = joinpath(SLIDE_DIR,"data")
+
+# Read bluenote output for comparison / to fill in data we have not yet saved.
+bn = merge(
+    read_from(joinpath(f_bench, "8b_bluenote_energy.yml"); run_bash=true),
+    read_from(joinpath(f_bench, "8b_bluenote_electricity.yml"); run_bash=true),
+    read_from(joinpath(f_bench, "8b_bluenote_emissions.yml"); run_bash=true),
+    read_from(joinpath(f_bench, "8b_bluenote_share.yml"); run_bash=true),
+    read_from("dev/windc_1.0/4_eem/8b_bluenote_chk"; run_bash=true),
+)
+
+# !!!! Add bluenote data where we have not yet saved EEM output and filter year.
+merge!(io_in, Dict(
+    :fvs => bn[:fvs],
+    :netgen => bn[:netgen],
+))
+io = Dict(k => filter_with(df, (yr=year,); drop=true) for (k,df) in io_in)
+
+# Read sets and edit as needed (!!!!). Eventually, this will come from the EEM output.
+# We will need to save the sets that correspond with each run stage (summary, scaled, EEM)
+# so we can maybe make one "read data" function, or option for "build" that build step and
+# get the associated info. We can do this by adding a method to build()
 set = merge(
     read_from(joinpath(f_read,"setlist.yml")),
     Dict(k=>df[:,1] for (k,df) in read_from(joinpath(f_data,"coresets","eem"))),
@@ -15,15 +41,6 @@ set = merge(
 set[:g] = unique(dropmissing(io[:ys0])[:,:g])
 set[:s] = unique(dropmissing(io[:ys0])[:,:s])
 set[:eneg] = ["col","ele","oil"]
-
-# Add bluenote data where we have not yet saved EEM output.
-merge!(io_in, Dict(
-    :fvs => bn[:fvs],
-    :netgen => bn[:netgen],
-))
-
-# Isolate year to loop over.
-io = Dict(k => filter_with(df, (yr=year,); drop=true) for (k,df) in io_in)
 
 # Calculate additional values for constraints.
 io[:va0] = io[:ld0] + io[:kd0]
@@ -39,11 +56,6 @@ d = Dict(k => convert_type(Dict, fill_zero(df; with=set)) for (k,df) in copy(io)
 SLiDE._calibration_set!(set)
 SLiDE.add_permutation!(set, (:r,:e))
 SLiDE.add_permutation!(set, (:r,:e,:g))
+SLiDE.add_permutation!(set, (:r,:e,:s))
 SLiDE.add_permutation!(set, (:r,:g,:e))
 SLiDE.add_permutation!(set, (:r,:m,:e))
-
-R = copy(set[:r])
-G = copy(set[:g])
-S = copy(set[:s])
-M = copy(set[:m])
-SNAT = setdiff(S,set[:eneg])
